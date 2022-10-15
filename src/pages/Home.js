bd6d9b313c6e225bfbd24c98ea3fed6e6e7725b9
Tanmay Bhosale <tanmaybhosale24@gmail.com>
2022-10-15 11:04:56 +0530

    initial commit ⚡

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,61 @@
+import { useState } from "react"
+import { useEffect } from "react"
+import supabase from "../config/supabaseClient"
+
+import SmoothieCard from "../components/SmoothieCard"
+
 const Home = () => {
+  const [fetchError,setFetchError] = useState(null)
+  const [smoothies,setSmoothies] = useState(null)
+  const [orderBy,setOrderBy] = useState("created_at")
+
+  const handleDelete = (id)=>{
+    setSmoothies(previousSmoothies => {
+      return previousSmoothies.filter(sm => sm.id !== id)
+    })
+  }
+
+  useEffect(()=>{
+    const fetchSmoothies = async ()=>{
+      const {data,error} = await supabase
+      .from("smoothies")
+      .select()
+      .order(orderBy,{ascending:false})
+
+      if(error){
+        setFetchError("Could not fetch smoothies")
+        setSmoothies(null)
+        console.log(error)
+      }
+
+      if(data){
+        setSmoothies(data)
+        setFetchError(null)
+      }
+    }
+
+    fetchSmoothies()
+  },[orderBy])
+
   return (
     <div className="page home">
-      <h2>Home</h2>
+      {fetchError && (<p>{fetchError}</p>)}
+      {smoothies && (
+        <div className="smoothies">
+          <div className="order-by">
+            <p>Order by:</p>
+            <button onClick={()=>setOrderBy("created_at")}>Time Created</button>
+            <button onClick={()=>setOrderBy("title")}>Title</button>
+            <button onClick={()=>setOrderBy("rating")}>Rating</button>
+            {orderBy}
+          </div>
+          <div className="smoothie-grid">
+            {smoothies.map(smoothie => (
+                <SmoothieCard key={smoothie.id} smoothie={smoothie} onDelete={handleDelete} />
+            ))}
+            </div>
+        </div>
+      )}
     </div>
   )
 }
